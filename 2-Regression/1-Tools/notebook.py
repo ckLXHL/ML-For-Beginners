c2504826eaaacbf78bfb18b8c07ed71d50904cb5
ckLXHL <cklxhl@gmail.com>
2021-07-19 23:55:45 +0800

    add py and start coding

---
+++ 2-Regression/1-Tools/notebook.py
@@ -0,0 +1,7 @@
+from sklearn import linear_model, model_selection, datasets
+import numpy as np
+import matplotlib.pyplot as plt
+
+X, y = datasets.load_diabetes(return_X_y=True)
+print(X.shape)
+print(X[0])
